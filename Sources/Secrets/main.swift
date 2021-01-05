5e4fa6708ef9214f5acfb56d1adb1c65bf13f2d8
Andrew Madsen <andrew@openreelsoftware.com>
2021-01-05 16:04:32 -0700

    Implement basic CLI app, without ArgumentParser

--- Sources/Secrets/main.swift
+++ Sources/Secrets/main.swift
@@ -1,3 +1,11 @@
+import Foundation
 import ROT
 
-print("Hello, World!")
+let arguments = ProcessInfo.processInfo.arguments
+if (arguments.count < 2) {
+    print("You must specify a string to convert\n")
+    exit(0)
+}
+
+let rot = ROTAlgorithm()
+print(rot.encode(arguments[1]))
