af0390f11ce540ecaf3ac8c14a92c48a26941e8c
Andrew Madsen <andrew@openreelsoftware.com>
2021-01-05 16:19:16 -0700

    Validate rotation distance

--- Sources/Secrets/GlobalOptions.swift
+++ Sources/Secrets/GlobalOptions.swift
@@ -11,4 +11,10 @@ import ArgumentParser
 struct GlobalOptions: ParsableArguments {
     @Option(help: "The rotation distance to use, from 1 to 25")
     var rotationDistance: Int = 13
+
+    func validate() throws {
+        guard (1...25).contains(rotationDistance) else {
+            throw ValidationError("Please specify a rotation distance between 1 and 25")
+        }
+    }
 }
